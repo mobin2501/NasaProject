3400d286089fc1f5bff463aefdc307d7b5287cf5
Mobin Rahman <mobin2501@gmail.com>
2019-10-06 21:27:10 +0600

    Version 1a

---
+++ Nasa Project/Assets/Scripts/MenuStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuStart: MonoBehaviour
+{
+    public Button strt;
+    private void Start()
+    {
+        strt.onClick.AddListener(MenStart);
+    }
+    void MenStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
