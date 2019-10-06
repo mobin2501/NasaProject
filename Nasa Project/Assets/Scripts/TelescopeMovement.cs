6fa86f4e1adb906b847fdc6c7b77e4560675b959
Mobin Rahman <mobin2501@gmail.com>
2019-10-06 22:49:08 +0600

    Version 1b

---
+++ Nasa Project/Assets/Scripts/TelescopeMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelescopeMovement : MonoBehaviour
+{
+    public Rigidbody tlscope;
+    bool three = false;
+    private void Start()
+    {
+       three = false;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            tlscope.AddForce(0f, 1f, 0f,ForceMode.Force);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            tlscope.AddForce(0f, -1f, 0f, ForceMode.Force);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            tlscope.AddForce(-1f, 0f, 0f, ForceMode.Force);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            tlscope.AddForce(1f, 0f, 0f, ForceMode.Force);
+        }
+        else if (Input.GetKey(KeyCode.Z) && three == true)
+        {
+            tlscope.AddForce(0f, 0f, 1f, ForceMode.Force);
+        }
+        else if (Input.GetKey(KeyCode.X)&& three==true)
+        {
+            tlscope.AddForce(0f, 0f, -1f, ForceMode.Force);
+        }
+        
+
+    }
+}
