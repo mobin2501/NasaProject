e6eb2d2a2d790fb162319ad00782a5226a8607e2
Mobin Rahman <mobin2501@gmail.com>
2019-10-19 01:08:50 +0600

    10

--- Nasa Project/Assets/Scripts/CamScript2.cs
+++ Nasa Project/Assets/Scripts/CamScript2.cs
@@ -8,6 +8,22 @@ public class CamScript2 : MonoBehaviour
     {
         float rot = StartRelativeVelocity.speed;
         cam.transform.Rotate(0f,(rot)*Time.fixedDeltaTime , 0f);
-        
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            cam.transform.Rotate(-0.5f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            cam.transform.Rotate(0.5f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            cam.transform.Rotate(0f, -.5f, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            cam.transform.Rotate(0f, 0.5f, 0f);
+        }
     }
 }
