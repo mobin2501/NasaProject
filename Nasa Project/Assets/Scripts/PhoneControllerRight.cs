aa8d0dec50b6fddbc7e529d57b5f318b1be5119d
Mobin Rahman <mobin2501@gmail.com>
2019-10-19 04:07:13 +0600

    13

--- Nasa Project/Assets/Scripts/PhoneControllerRight.cs
+++ Nasa Project/Assets/Scripts/PhoneControllerRight.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine;
+using System.Collections;
 
-public class PhoneControllerRight : MonoBehaviour
+public class PhoneControllerRight : MonoBehaviour , IPointerDownHandler
 {
     public Button bt;
     public Transform ts;
     public Vector3 w;
     public Vector3 a;
-    private void Start()
+    bool down = false;
+    /*private void Start()
     {
         bt.onClick.AddListener(D);
+    }*/
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        D();
     }
     public void D()
     {
-        ts.transform.position = ts.transform.position - a * Time.fixedDeltaTime;
+        ts.transform.position = ts.transform.position + a * Time.fixedDeltaTime;
     }
 }
