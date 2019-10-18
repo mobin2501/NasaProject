196f32f43d2d2764305efd991f4a9c87f4bf1cdb
Mobin Rahman <mobin2501@gmail.com>
2019-10-19 00:09:28 +0600

    9

--- Nasa Project/Assets/Scripts/CamScript.cs
+++ Nasa Project/Assets/Scripts/CamScript.cs
@@ -5,7 +5,7 @@ public class CamScript : MonoBehaviour
 {
     public Transform ts;
     public Transform campos;
-    Vector3 offset = new Vector3(0f, 30f, 0f);
+    public Vector3 offset = new Vector3(0f, 30f, 0f);
     private void Start()
     {
        // campos.Rotate(0f, 0f, 0f);
---
+++ Nasa Project/Assets/Scripts/CamScript2.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class CamScript2 : MonoBehaviour
+{
+    public Transform cam;
+    private void FixedUpdate()
+    {
+        float rot = StartRelativeVelocity.speed;
+        cam.transform.Rotate(0f,(rot)*Time.fixedDeltaTime , 0f);
+        
+    }
+}
